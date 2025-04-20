637e2eba28a91bc8477cd7dc757f5c5be96d47c4
Bravodum <bravodumov@gmail.com>
2025-04-20 23:51:55 +0300

    Initial migration

---
+++ ProductService/ProductService.API/Extensions/HostExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Infrastructure.Persistence;
+
+namespace ProductService.API.Extensions;
+
+public static class HostExtensions
+{
+    public static IHost MigrateDatabase(this IHost host)
+    {
+        using var scope = host.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+
+        db.Database.Migrate(); // применит все миграции
+
+        return host;
+    }
+}
--- ProductService/ProductService.API/Program.cs
+++ ProductService/ProductService.API/Program.cs
@@ -1,73 +1,50 @@
+using ProductService.API.Extensions;
 using ProductService.API.Services;
 using ProductService.Infrastructure.Bootstrap;
 
-namespace ProductService.API
+namespace ProductService.API;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
+        CreateHostBuilder(args)
+            .Build()
+            .MigrateDatabase()  // добавили миграцию красиво
+            .Run();
+    }
 
-            // Можно сделать миграции, сиды и т.п. здесь при старте, если нужно
-
-            host.Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                // можно накинуть .env, secrets.json, etc.
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureServices((context, services) =>
                 {
-                    // можно накинуть .env, secrets.json, etc.
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
+                    services.AddGrpc();
+
+                    services.AddInfrastructure(context.Configuration);
+                });
+
+                webBuilder.Configure(app =>
                 {
-                    webBuilder.ConfigureServices((context, services) =>
+                    var env = app.ApplicationServices.GetRequiredService<IHostEnvironment>();
+
+                    if (env.IsDevelopment())
                     {
-                        services.AddGrpc();
+                        // какие-нибудь middleware, если нужны
+                    }
 
-                        services.AddInfrastructure(context.Configuration);
-                    });
+                    app.UseRouting();
 
-                    webBuilder.Configure(app =>
+                    app.UseEndpoints(endpoints =>
                     {
-                        var env = app.ApplicationServices.GetRequiredService<IHostEnvironment>();
-
-                        if (env.IsDevelopment())
-                        {
-                            // какие-нибудь middleware, если нужны
-                        }
-
-                        app.UseRouting();
-
-                        app.UseEndpoints(endpoints =>
-                        {
-                            endpoints.MapGrpcService<ProductGrpcService>();
-                            endpoints.MapGet("/", () => "ProductService gRPC is running");
-                        });
+                        endpoints.MapGrpcService<ProductGrpcService>();
+                        endpoints.MapGet("/", () => "ProductService gRPC is running");
                     });
                 });
-    }
+            });
 }
-
-//namespace ProductService.API
-//{
-//    public class Program
-//    {
-//        public static void Main(string[] args)
-//        {
-//            var builder = WebApplication.CreateBuilder(args);
-
-//            // Add services to the container.
-//            builder.Services.AddGrpc();
-
-//            var app = builder.Build();
-
-//            // Configure the HTTP request pipeline.
-//            app.MapGrpcService<GreeterService>();
-//            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-//            app.Run();
-//        }
-//    }
-//}
